9ad1ddbe765f0d1b2bd31baf46b3f4ee15452da0
Aditya019 <adityapande019@gmail.com>
2026-06-04 16:40:05 +0530

    feat: add admin dashboard APIs for users, stores and stats

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import {PrismaClient}from '@prisma/client';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
+import adminRoutes from './routes/admin.js';
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth',authRoutes);
+app.use('/api/admin',adminRoutes);
 
 app.get('/',(req,res)=>{
     res.send("Roxiler Store Rating API is running with ES Modules!");
